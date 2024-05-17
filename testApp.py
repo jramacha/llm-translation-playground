479ac24c7e7fbdd8d796e7ec832d88fa9f7221ce
ajeebpeter <67509657+ajeebpeter@users.noreply.github.com>
2024-05-17 06:49:19 -0400

    first commit

---
+++ testApp.py
@@ -0,0 +1,256 @@
+
+import streamlit as st
+import json,os
+import boto3
+import logging
+from botocore.exceptions import ClientError
+from tmxFileProcess import (
+    processTMXFile,
+    loadEmbeddings,
+    getExamples,
+    populateRuleLanguageLookup,
+)
+
+logger = logging.getLogger(__name__)
+
+
+st.title("Language Translator with LLMs")
+text2translate=st.text_area("Text To translate")
+
+col1, col2 = st.columns(2)
+
+#Language Choices
+st.header("Translation options")
+LAN_CHOICES = {"EN": "English", "FR": "French", "ES": "Spanish", "DE": "German"}
+def format_func(option):
+    return LAN_CHOICES[option]
+
+lcol1, lcol2 = st.columns(2)
+with lcol1:
+  sl=st.selectbox("Select Source Language",options=list(LAN_CHOICES.keys()), format_func=format_func)
+with lcol2:
+  tl=st.selectbox("Select Target Language",options=list(LAN_CHOICES.keys()), format_func=format_func)
+
+  
+
+#Translator Model Choices
+MODEL_CHOICES = {"anthropic.claude-3-sonnet-20240229-v1:0": "Claude 3 Sonnet", "anthropic.claude-3-haiku-20240307-v1:0": "Claude 3 Haiku", "mistral.mistral-large-2402-v1:0": "Mistral", "cohere.embed-multilingual-v3": "Cohere"}
+def format_func(option):
+    return MODEL_CHOICES[option]
+model_id=st.selectbox("Select LLM models from Amazon Bedrock",options=list(MODEL_CHOICES.keys()), format_func=format_func)
+
+
+st.header("Influence options")
+#TMX Examples Files
+filename = st.file_uploader("Upload a TMX file", type=["tmx"])
+st.write('You selected `%s`' % filename)
+
+#Embedding Model Choices
+EMBED_CHOICES = {"amazon.titan-embed-text-v2:0": "Titan Embedding Text v2", "cohere.embed-multilingual-v3": "Cohere Multilingual", "cohere.embed-english-v3": "Cohere English"}
+
+def format_func(option):
+    return EMBED_CHOICES[option]
+embedding_id=st.selectbox("Select Embedding models from Amazon Bedrock",options=list(EMBED_CHOICES.keys()), format_func=format_func)
+
+
+def getExamplesXml(sl,tl,examples):   
+    xml_out = "\n"
+    for example in examples:
+        xml_out += f"<example>\n<source>{example[sl]}</source>\n<target>{example[tl]}</target>\n</example>\n"
+    return xml_out
+
+def getRules(sl,tl):
+  print(sl, tl)
+  tmx_db=st.session_state.tmx_db
+  matching_rules = tmx_db.similarity_search(text2translate, filter={"lang": sl})
+  st.session_state.matching_rules=matching_rules 
+  return getExamples(sl,tl,st.session_state.rule_language_lookup ,matching_rules)
+
+
+session_state = st.session_state
+examples= []
+egcol1, egcol2 = st.columns(2)
+with egcol2:  
+  if st.button("Process TMX File"):
+    documents=processTMXFile(filename)
+    tmx_db = loadEmbeddings(documents)
+    st.session_state.tmx_db = tmx_db
+    rule_language_lookup=populateRuleLanguageLookup(documents)
+    st.session_state.rule_language_lookup = rule_language_lookup
+    examples = getRules(sl,tl)
+      
+with egcol1:
+    st.text("")
+
+
+if st.button("Collect Matching Rules"):
+    examples=getRules(sl,tl)
+
+
+exampleText=""
+for example in examples:
+  exampleText+= example[sl] + " : "
+  exampleText+= example[tl]+ "\n"
+  
+
+st.text(exampleText)
+
+
+example_xml=getExamplesXml(sl,tl,examples)
+
+custom_examples=st.text_area("Custom Examples: "+ LAN_CHOICES[sl] + " : " +LAN_CHOICES[tl] +"\n")
+st.write("One example pair per line seperated by colon (:). Examples below")
+st.write("Hello, how are you? : Hola, ¿cómo estás?")
+
+
+def dict_to_xml(examples):
+    xml_out = "\t\n"
+    # Split each line on a colon and print the result
+    for line in examples:
+        if ":" in line:
+            parts = line.split(":", 1)
+            xml_out += f"<example>\n\t<source>{parts[0]}</source>\n\t<target>{parts[1]}</target>\n</example>\n"
+    xml_out += "\n"
+    return xml_out
+
+custom_example_xml=""
+if custom_examples.strip() != ""  :
+  # Split the string on newlines
+  lines = custom_examples.split("\n")
+  custom_example_xml=dict_to_xml(lines)
+  print(custom_example_xml)
+
+
+
+prompt = """
+<prompt>
+  <system_instructions>
+    You are an expert language translator assistant. You will be given text in one language, and you need to translate it into another language. You should maintain the same tone, style, and meaning as the original text in your translation.
+  </system_instructions>
+
+
+  <source_language>
+    <language_name>%(sl)s</language_name>
+  </source_language>
+
+
+  <target_language>
+    <language_name>%(tl)s</language_name>
+  </target_language>
+
+
+  <input_text>
+    %(text2translate)s
+  </input_text>
+
+
+  <examples>
+    %(custom_example_xml)s
+    %(example_xml)s
+  </examples>
+
+
+  <translation_rules>
+    <rule>
+      <description>Translate product names literally</description>
+      <data_map>
+        <item>
+          <source>MIS</source>
+          <target>Ratings</target>
+        </item>
+        <item>
+          <source>language</source>
+          <target>bhasha</target>
+        </item>
+      </data_map>
+    </rule>
+    <rule>
+      <description>Translate company names literally</description>
+      <data_map>
+        <item>
+          <source>Apple Inc.</source>
+          <target>Apple Inc.</target>
+        </item>
+        <item>
+          <source>Google LLC</source>
+          <target>Google LLC</target>
+        </item>
+      </data_map>
+    </rule>
+  </translation_rules>
+
+
+  <instructions>
+    Translate the text in the <input_text> tags from {{SOURCE_LANGUAGE}} to {{TARGET_LANGUAGE}}. refer the examples provoded in the <Examples> section  and translation rules specified in the <translation_rules> section. Your translation should be enclosed in <translation></translation> tags.
+  </instructions>
+</prompt>
+"""
+
+
+client = boto3.client(
+        service_name="bedrock-runtime", region_name="us-east-1"
+    )
+
+def invokeBedrock(sl, tl, text2translate,prompt):
+    
+    data = { 'sl':sl, 'tl':tl, 'text2translate':text2translate , 'custom_example_xml':custom_example_xml , 'example_xml':example_xml}
+    xml_prompt = prompt%data
+    
+    # Initialize the Amazon Bedrock runtime client
+    print(xml_prompt);
+    # Invoke Claude 3 with the text prompt
+    
+
+    data = {
+        "max_tokens": 1000,
+        "messages": [
+            {"role": "user", "content": xml_prompt}
+        ],
+        "anthropic_version": "bedrock-2023-05-31"
+        }
+
+    try:
+      response = client.invoke_model(
+          modelId=model_id,
+          body=json.dumps(data),
+      )
+      return response
+
+    except ClientError as err:
+      logger.error(
+          "Couldn't invoke %s. Here's why: %s: %s",
+          model_id,
+          err.response["Error"]["Code"],
+          err.response["Error"]["Message"],
+      )
+      raise
+
+
+if st.button("Translate"):
+  response=invokeBedrock(sl,tl,text2translate,prompt);
+
+  # Process and print the response
+  result = json.loads(response.get("body").read())
+  input_tokens = result["usage"]["input_tokens"]
+  output_tokens = result["usage"]["output_tokens"]
+  output_list = result.get("content", [])
+
+  print("Invocation details:")
+  print(f"- The input length is {input_tokens} tokens.")
+  print(f"- The output length is {output_tokens} tokens.")
+
+  print(f"- The model returned {len(output_list)} response(s):")
+  for output in result.get("usage",[]):
+      print(output)
+
+  for output in output_list:
+      print(output["text"])
+
+  translated2Text = {    
+              output_list[0]["text"]
+          }
+  st.write(translated2Text)
+
+  st.write(f"- The input length is {input_tokens} tokens.")
+  st.write(f"- The output length is {output_tokens} tokens.")
+
---
+++ tmxFileProcess.py
@@ -0,0 +1,106 @@
+from bs4 import BeautifulSoup
+from langchain_core.documents import Document
+from langchain_community.vectorstores import FAISS
+import uuid
+
+
+
+def loadTMXFile(tmx_file):
+    return BeautifulSoup(open(tmx_file), "lxml")
+
+def processTMXFile(tmx_file):
+    #ToDo - Should be deleted 
+    tmx_file = "assets/ECDC.tmx"
+
+    tmx_soup=loadTMXFile(tmx_file)
+    return loadDocuments(tmx_soup)
+    
+
+
+def loadDocuments(tmx_soup):
+    documents = []
+
+    tu_tags = tmx_soup.find_all("tu")
+
+    for id,tu_tag in enumerate(tu_tags):
+        rule_id = id
+        tuv_tags = tu_tag.find_all("tuv")
+        
+        for tuv_tag in tuv_tags:
+            lang = tuv_tag["xml:lang"]
+            text = tuv_tag.find("seg").get_text()
+            
+            document = Document(page_content=text, metadata={"rule_id": rule_id, "lang": lang})
+            
+            documents.append(document)
+    return documents
+
+
+def loadEmbeddings(documents):
+    import pickle
+    from io import BytesIO
+    from pathlib import Path
+    from langchain.embeddings import BedrockEmbeddings
+    from langchain_community.vectorstores import FAISS
+    import boto3
+
+    boto3_session=boto3.session.Session()
+    bedrock_runtime = boto3_session.client("bedrock-runtime")
+    embedding_modelId = "cohere.embed-multilingual-v3"
+
+    # will be used to embed user queries
+    query_embed_model = BedrockEmbeddings(
+        model_id=embedding_modelId,
+        model_kwargs={"input_type": "search_query"},
+        client=bedrock_runtime,
+    )
+
+
+    # will be used to embed documents
+    doc_embed_model = BedrockEmbeddings(
+        model_id=embedding_modelId,
+        model_kwargs={"input_type": "search_document", "truncate": "END"},
+        client=bedrock_runtime,
+    )
+
+    vector_store_file = "tmx_vec_db.pkl"
+    CREATE_NEW = False # create new vector store if True or load existing one if False
+    DOCS_TO_INDEX = 3000 # number of documents to index, this code does about 700 docs per minute and the are over 64K docs in the tmx file
+
+    if CREATE_NEW:
+        tmx_db = FAISS.from_documents(documents[:DOCS_TO_INDEX], doc_embed_model)
+        tmx_db.embedding_function = query_embed_model
+        pickle.dump(tmx_db.serialize_to_bytes(), open(vector_store_file, "wb"))
+    else:
+        if not Path(vector_store_file).exists():
+            raise FileNotFoundError(f"Vector store file {vector_store_file} not found. Set CREATE_NEW to True to create a new vector store.")
+        
+        vector_db_buff = BytesIO(pickle.load(open(vector_store_file, "rb")))
+        tmx_db = FAISS.deserialize_from_bytes(serialized=vector_db_buff.read(), embeddings=query_embed_model)
+
+    return tmx_db
+
+
+
+
+def populateRuleLanguageLookup(documents) :
+    from collections import defaultdict
+    rule_language_lookup = defaultdict(dict)
+    for  docs in documents:
+        rule_language_lookup[docs.metadata["rule_id"]].update({docs.metadata["lang"]: docs.page_content})
+    return rule_language_lookup
+
+
+def getExamples(source_lang,target_lang,rule_language_lookup,matching_rules):
+    examples = []
+    print(len(matching_rules))
+    print(len(rule_language_lookup))
+    # loop first 10 in rule_language_lookup
+
+    for rule in matching_rules:
+        example = {source_lang: rule.page_content, target_lang: rule_language_lookup[rule.metadata["rule_id"]][target_lang]}
+        examples.append(example)
+        print(example)
+
+    return examples
+
